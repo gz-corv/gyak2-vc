940ce22b1a8dad13d2e08e3b6078de671421b67c
gz-corv <gergely.zalai@stud.uni-corvinus.hu>
2022-11-12 09:17:14 +0100

    model classes country and ramen added

---
+++ Gyak03_Linq/Gyak03_Linq/Country.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gyak03_Linq
+{
+    class Country
+    {
+
+        public int ID { get; set; }
+
+        public String Name { get; set; }
+    }
+}
---
+++ Gyak03_Linq/Gyak03_Linq/Ramen.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gyak03_Linq
+{
+    class Ramen
+    {
+        public int ID { get; set; }
+        public String Brand { get; set; }
+
+        public String Name { get; set; }
+
+        public int CountryFK { get; set; }
+
+        public Country Country { get; set; }
+
+        public double Rating { get; set; }
+    }
+}
