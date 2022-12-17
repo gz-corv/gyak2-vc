3c5af82a160568b679d1bbe5aacf5f8b4c3b740d
gz-corv <dy7sx3@SZA0030925>
2022-12-17 13:36:42 +0100

    create portfolios, populate with items, and display it in a datagridview

--- Gyak07/Gyak07/Form1.cs
+++ Gyak07/Gyak07/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Gyak07.Entities;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,12 +16,28 @@ namespace Gyak07
 
         private PortfolioEntities context = new PortfolioEntities();
         private List<Tick> ticks;
+        private List<PortfolioItem> portfolioItems;
+
         public Form1()
         {
             InitializeComponent();
 
             ticks = context.Tick.ToList();
             dataGridView1.DataSource = ticks;
+            CreatePortfolio();
+        }
+
+        private void CreatePortfolio()
+        {
+            if (portfolioItems == null)
+                portfolioItems = new List<PortfolioItem>();
+
+            portfolioItems.Add(new PortfolioItem() { Index = "OTP", Volume = 10 });
+            portfolioItems.Add(new PortfolioItem() { Index = "ZWACK", Volume = 10 });
+            portfolioItems.Add(new PortfolioItem() { Index = "ELMU", Volume = 10 });
+
+            dataGridView2.DataSource = portfolioItems;
+
         }
     }
 }
